76d0dc2c065f09649ffe2d0f382f9780506ac961
nadianielsen <U2404561@rts-365.dk>
2023-05-08 13:41:14 +0200

    made the navigation

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,7 +1,9 @@
+import Navigation from "./Navigation";
 
 const Footer = () => {
     return ( 
         <footer className="w-full h-24 bg-white fixed bottom-0 shadow-2xl shadow-neutral-500">
+        <Navigation />
         </footer>
      );
 }
