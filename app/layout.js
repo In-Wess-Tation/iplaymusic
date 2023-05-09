53d2a8dd3da4bac0d9997d6c104675337f5278d6
nadianielsen <U2404561@rts-365.dk>
2023-05-09 09:03:07 +0200

    made a header component

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import Footer from './components/Footer'
+import Header from './components/Header'
 import './globals.css'
 import { Poppins } from 'next/font/google'
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
+        <Header />
         {children}
         <Footer />
         </body>
