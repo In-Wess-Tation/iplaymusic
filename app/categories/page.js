72777ba99a146b8f91c85bc3620674bf9269b4c2
nadianielsen <U2404561@rts-365.dk>
2023-05-10 09:08:41 +0200

    categories is half done for now - made color props

--- app/categories/page.js
+++ app/categories/page.js
@@ -3,15 +3,17 @@ import CategoriesProp from "../components/CategoriesProp";
 const Categories = () => {
 
     return ( 
-        <main className="grid gap-y-20">
-            <h1 className="text-3xl font-bold pl-12 py-6 text-transparent bg-clip-text bg-gradient-to-t from-[#ee0979] to-[#ff6a00]">Categories</h1>
-            <CategoriesProp headline={"Alternative"} className="bg-pink-600"/>
-            <CategoriesProp headline={"Alternative"} />
-            <CategoriesProp headline={"Alternative"} />
-            <CategoriesProp headline={"Alternative"} />
-            <CategoriesProp headline={"Alternative"} />
-            <CategoriesProp headline={"Alternative"} />
-            <CategoriesProp headline={"Alternative"} />
+        <main className="flex flex-col gap-y-4">
+            <h1 className="text-3xl font-bold pl-12 py-4 text-transparent bg-clip-text bg-gradient-to-r from-[#ee0979] to-[#ff6a00]">Categories</h1>
+            <CategoriesProp headline={"Alternative"} bgprimary="bg-[#FF1168]"/>
+            <CategoriesProp headline={"Blues"} bgprimary="bg-[#E54028]"/>
+            <CategoriesProp headline={"Classical"} bgprimary="bg-[#F18D05]"/>
+            <CategoriesProp headline={"Country"} bgprimary="bg-[#F2BC06]"/>
+            <CategoriesProp headline={"Dance"} bgprimary="bg-[#5EB11C]"/>
+            <CategoriesProp headline={"Electronic"} bgprimary="bg-[#3A7634]"/>
+            <CategoriesProp headline={"Fitness"} bgprimary="bg-[#0ABEBE]"/>
+            <CategoriesProp headline={"Hip-Hop/Rap"} bgprimary="bg-[#00A1CB]"/>
+            <CategoriesProp headline={"Industrial"} bgprimary="bg-[#115793]"/>
         </main>
      );
 }
