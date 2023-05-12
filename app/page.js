0e347c16e0800dd9413d37a56cd4cf384edf9d60
nadianielsen <U2404561@rts-365.dk>
2023-05-12 08:37:39 +0200

    fixing some gradient on text

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default async function Home() {
 
   return (
     <main className="grid">
-      <h1 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#ee0979] to-[#ff6a00] pl-12 py-2 w-[40%]">Featured</h1>
+      <h1 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#ee0979] to-[#ff6a00] pl-12 py-2">Featured</h1>
         <FeaturedCards />
     </main>
   )
