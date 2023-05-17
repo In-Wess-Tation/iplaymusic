aa082788899a91f30c308b09b65d5fb59f194b02
nadianielsen <U2404561@rts-365.dk>
2023-05-17 08:41:05 +0200

    Got overflow-x-scroll to work at latesttrends

---
+++ app/components/TrendingBox.js
@@ -0,0 +1,11 @@
+
+
+const TrendingBox = ({genre}) => {
+    return ( 
+        <div className="bg-black w-[45%] h-[32] min-w-[45%] mr-2">
+            <h2>{genre}</h2>
+        </div>
+     );
+}
+ 
+export default TrendingBox;
