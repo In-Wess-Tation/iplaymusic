91d3cb4d073d3bee352dc202d35b4c3eca744254
nadianielsen <U2404561@rts-365.dk>
2023-05-24 09:19:08 +0200

    typo in searchParams and fixed it

--- app/callback/page.js
+++ app/callback/page.js
@@ -12,10 +12,9 @@ const Callback = () => {
     const [token, setToken] = useContext(TokenContext);
     const router = useRouter();
 
-    console.log(setToken)
 
-    const seachParams = useSearchParams();
-    const code = seachParams.get("code");
+    const searchParams = useSearchParams();
+    const code = searchParams.get("code");
     console.log(code)
 
     useEffect(() => {
@@ -29,23 +28,7 @@ const Callback = () => {
             router.push("/featured")
         })
         .catch(err => console.log(err))
-        /* fetch("http://localhost:3000/api/auth", {
-            method: "POST",
-            body: JSON.stringify({ code })
-        })
-        .then(response => {
-            if (response.status === 201) {
-                return response.json()
-            }
-            if (response.status === 500) {
-                return response.text()
-            }
-        })
-        .then(result => {
-            console.log(result)
-            // localStorage.setItem("access", JSON.stringify(result))
-            // cookies().set("token", response.data.access_token)
-        }) */
+     
     }, []);
 
     return null;
@@ -54,26 +37,3 @@ const Callback = () => {
 export default Callback;
 
 
-
-// import { useSearchParams } from "next/navigation";
-// import { useEffect } from "react";
-
-// const Callback = () => {
-
-//     const searchParams = useSearchParams();
-//     const code = searchParams.get("code");
-//     console.log(code)
-
-//     useEffect(() => {
-//         fetch("http://localhost:3000/api/auth", {
-//             method: "POST",
-//             body: JSON.stringify({ code })
-//         })
-//         .then(response => response.json())
-//         .then(result => console.log(result))
-//     }, []);
-
-//     return null;
-// }
- 
-// export default Callback;
--- app/contexts/TokenContext.js
+++ app/contexts/TokenContext.js
@@ -1,5 +1,4 @@
 "use client";
-// import { createContext, useState, useEffect } from "react";
 import { createContext } from "react";
 
 
@@ -8,54 +7,3 @@ import { createContext } from "react";
 
 
 
-// const AuthProvider = (props) => {
-    
-//     const [token, setToken] = useState();
-// //  const [clientId, setClientId] = useState(process.env.CLIENT_ID);
-// //  const [clientSecret, setClientSecret] = useState(process.env.CLIENT_SECRET);
-
-// const id = process.env.NEXT_PUBLIC_CLIENT_ID; 
-// const secret = process.env.NEXT_PUBLIC_CLIENT_SECRET;
-
-// // const [data, setData] = useState();
-
-//     // useEffect(() => {
-//     //     fetch("https://accounts.spotify.com/api/token", {
-//     //     method: "POST",
-//     //     headers: {
-//     //       "Content-Type": "application/x-www-form-urlencoded",
-//     //       "Authorization": "Basic " + btoa(id + ":" + secret)
-//     //     },
-//     //     body: "grant_type=client_credentials",
-//     // })
-//     //   .then(response => response.json())
-//     //   .then(result => setToken(`${result.token_type} ${result.access_token}`))
-
-//     // }, []);
-
-//     useEffect(() => {
-//         fetch("https://accounts.spotify.com/api/token", {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/x-www-form-urlencoded",
-//                 "Authorization": "Basic " + btoa(id + ":" + secret)
-//             },
-//             body: "grant_typw=client_credentials",
-//         })
-//         .then(respone => respone.json())
-//         .then(result => setToken(result.access_token));
-
-//     }, []);
-
-//     token && console.log(token)
-    
-//     return ( 
-//         <AuthContext.Provider value={{ token }}>
-//             {props.children}
-//         </AuthContext.Provider>
-//      );
-     
-//     };
-
-//      export default AuthProvider;
-     
