338f49540cd552eda0a97708938a8bfe99c6ae74
nadianielsen <U2404561@rts-365.dk>
2023-05-25 09:42:53 +0200

    added priority on featuredCards - since there's a warning

--- app/components/FeaturedCards.js
+++ app/components/FeaturedCards.js
@@ -6,7 +6,7 @@ import Link from "next/link";
 const FeaturedCards = ({url}) => {
     return ( 
         <Link href={"/"} className="w-72 h-80 bg-gradient-to-b from-white to-black rounded-2xl my-4 flex flex-col justify-end place-self-center relative">
-            <Image src={url} alt="mflkp" width={1000} height={1000} className="absolute w-full h-full rounded-2xl"/>
+            <Image src={url} alt="mflkp" width={1000} height={1000} className="absolute w-full h-full rounded-2xl" priority/>
         </Link>
      );
 }
