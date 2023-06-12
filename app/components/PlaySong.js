4ffd5f9bf0d8dcb3582f2c52bfc823183bf5e392
nadianielsen <U2404561@rts-365.dk>
2023-06-12 10:38:24 +0200

    ternary operater with fapause and faplay in playsong.js

--- app/components/PlaySong.js
+++ app/components/PlaySong.js
@@ -26,7 +26,7 @@ const PlaySong = ({ song, artist, time, id }) => {
             <div className="flex gap-x-5">
                
                 <button onClick={playing ? pause : play} className="bg-gradient-to-t from-pink-yyyy to-orange w-8 h-8 rounded-full flex justify-center items-center">
-                    <FaPlay className="text-xs text-white" />
+                    {playing ? <FaPause className="text-xs text-white"/> : <FaPlay className="text-xs text-white" />}
                 </button>
 
                 <div>
